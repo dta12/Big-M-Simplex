99d5fff377aad8d46064262f1ab75e82a83049f4
unknown <dta12@pitt.edu>
2023-11-25 23:58:33 -0500

    implemented the simplex algorithm

--- simplex.c
+++ simplex.c
@@ -1,5 +1,16 @@
+#include <stdio.h>
 #include "tabelau.h"
 #include "float.h"
+
+// to make things pretty 
+
+void print_line(int width, char separator) {
+    for (int i = 0; i < width; i++) {
+        printf("%c", separator);
+    }
+    printf("\n");
+}
+
 /*
     - return -1 if no pivot column can be found
     otherwise return the column index of the entry with minimum value
@@ -42,7 +53,7 @@ int find_pivot_row(s_tableau *table, int c){
 void apply_pivot(s_tableau *table, int r, int c){
     // Divide the pivot row by the pivot element
     float pivot_element = table->table[r][c].value;
-    for (int i = 0; i < table->num_rows; i++) {
+    for (int i = 0; i < table->num_cols; i++) {
         table->table[r][i].value /= pivot_element;
     }
 
@@ -55,25 +66,63 @@ void apply_pivot(s_tableau *table, int r, int c){
             }
         }
     }
+    // update the max value
+    table->objective_p = table->table[table->num_rows - 1][table->num_cols - 1].value;
+}
+
+void apply_simplex(s_tableau *table){
+    int iteration = 0;
+    printf("%s\n", "Initial Tabelau:");
+    while(!table->is_finished){
+        print_tabelau(table);
+        print_line(8*table->num_cols, '-');
+        int piv_col = find_pivot_col(table);
+        if(piv_col == -1){
+            printf("%s\n", "The optimal solution has been found");
+            table->is_finished = true;
+            print_tabelau(table);
+        }else{
+            int piv_row = find_pivot_row(table, piv_col);
+            if(piv_row == -1){
+                printf("%s\n", "No optimal solution exists");
+                table->is_finished = true;
+                print_tabelau(table);
+            }
+            apply_pivot(table, piv_row, piv_col);
+            iteration++;
+            printf("Iteration: %d \tPivot chosen: (r: %d, c: %d)\n", iteration, piv_row + 1, piv_col + 1);
+        }
+    }
 }
 
 int main() {
     char equations[MAX_EQUATIONS][100];
 
-    // Example: Input equations
-    strcpy(equations[0], "x + y <= 10");
-    strcpy(equations[1], "-x + y >= 2");
-    strcpy(equations[2], "-2x - y + p = 0");
+    FILE *f = fopen("eq.txt", "r");
+
+    if(f == NULL){
+        perror("Error opening the equation file");
+        return 1;
+    }
+    char buffer[100];
+    int counter = 0;
+    while (fgets(buffer, sizeof(buffer), f) != NULL) {
+        // Process each equation/inequality line by line
+        strcpy(equations[counter], buffer);
+        counter++;
+    }
+    
+    // setup the token object
     s_tokens *tokens;
     initialize_token_object(&tokens);
     tokenize(tokens, 3, equations);
-    //print_tokens(tokens);
 
+    // setup the tabelau
     s_tableau *table;
     initialize_tabelau_object(&table);
     fill_tabelau(table, tokens);
-    print_tabelau(table);
-    apply_pivot(table, 1, 0);
-    print_tabelau(table);
+
+    // apply the simplex method to solve
+    apply_simplex(table);
     return 0;
 }
